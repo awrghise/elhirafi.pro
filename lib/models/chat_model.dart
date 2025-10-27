9f820139d8bf4a9ba0ba6d20beae5dd1b20c0f28
awrghise <awrghise@gmail.com>
2025-10-27 23:58:42 +0100

    Update chat_model.dart

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -4,19 +4,21 @@ class ChatModel {
   final String id;
   final List<String> participants;
   final Map<String, dynamic> participantNames;
+  final Map<String, dynamic> participantPhones; // <-- الحقل الجديد
   final DateTime lastMessageTime;
   final String lastMessageContent;
   final String lastMessageSenderId;
-  final int messageCount; // <-- الحقل الجديد
+  final int messageCount;
 
   ChatModel({
     required this.id,
     required this.participants,
     required this.participantNames,
+    required this.participantPhones, // <-- الحقل الجديد
     required this.lastMessageTime,
     required this.lastMessageContent,
     required this.lastMessageSenderId,
-    this.messageCount = 0, // <-- القيمة الافتراضية
+    this.messageCount = 0,
   });
 
   factory ChatModel.fromFirestore(DocumentSnapshot doc) {
@@ -25,10 +27,11 @@ class ChatModel {
       id: doc.id,
       participants: List<String>.from(data['participants'] ?? []),
       participantNames: Map<String, dynamic>.from(data['participantNames'] ?? {}),
+      participantPhones: Map<String, dynamic>.from(data['participantPhones'] ?? {}), // <-- الحقل الجديد
       lastMessageTime: (data['lastMessageTime'] as Timestamp).toDate(),
       lastMessageContent: data['lastMessageContent'] ?? '',
       lastMessageSenderId: data['lastMessageSenderId'] ?? '',
-      messageCount: data['messageCount'] ?? 0, // <-- قراءة الحقل الجديد
+      messageCount: data['messageCount'] ?? 0,
     );
   }
 
@@ -36,10 +39,11 @@ class ChatModel {
     return {
       'participants': participants,
       'participantNames': participantNames,
+      'participantPhones': participantPhones, // <-- الحقل الجديد
       'lastMessageTime': Timestamp.fromDate(lastMessageTime),
       'lastMessageContent': lastMessageContent,
       'lastMessageSenderId': lastMessageSenderId,
-      'messageCount': messageCount, // <-- إضافة الحقل الجديد
+      'messageCount': messageCount,
     };
   }
 }
